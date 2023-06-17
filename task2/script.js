6060e51c28f3f5d19bda59578ec21bf7359fe565
IhorStron <worklife0025@gmail.com>
2023-06-18 00:12:26 +0300

    fix task2

--- task2/script.js
+++ task2/script.js
@@ -1,34 +1,25 @@
 
-let n = parseInt(prompt("Введіть число N:"));
+let N = parseInt(prompt("Введіть число N (ціле число більше 0):"));
+while (isNaN(N) || !Number.isInteger(N)) {
+  N = parseInt(prompt("Некоректне значення. Введіть ціле число N:"));
+}
 
-
-if (isNaN(n)) {
-  console.log("Введено некоректне значення для N");
-  
+let M = parseInt(prompt("Введіть число M (ціле число більше N):"));
+while (isNaN(M) || M <= N || !Number.isInteger(M)) {
+  M = parseInt( prompt("Некоректне значення. Введіть ціле число M (більше за N:"));
 }
 
 
-let m = parseInt(prompt("Введіть число M:"));
-
-
-if (isNaN(m)) {
-  console.log(prompt("Введено некоректне значення для M"));
-  
-}
-
-
-let skipEven = confirm("Пропускати парні числа?");
-
+const skipEven = confirm("Пропускати парні числа?");
 
 let sum = 0;
 
-
-for (let i = n; i <= m; i++) {
+for (let i = N; i <= M; i++) {
   if (skipEven && i % 2 === 0) {
     continue; 
-  } else
+  }
+
   sum += i;
 }
 
-
-console.log("Сума чисел:", sum);
+console.log("Сума чисел:", sum); 
